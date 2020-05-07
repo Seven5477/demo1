419481b868259fec18e4536c9fde120da46d5892
Seven5477 <1530011482@qq.com>
2020-05-07 18:07:51 +0800

    更改样式

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,41 +1,46 @@
 <template>
   <div>
-      <ul>
-          <span @click="userinfo()">{{user}}
-          <span @click="logout()">退出</span>
+      <div class="index">学生管理系统
+          <span id="user" @click="userinfo()">{{user}}
+          <span id="logout" @click="logout()">退出</span>
           </span>
           <!-- <router-view /> -->
           <!-- <li>
             <router-link :to="{name: 'user'}">个人中心</router-link>
           </li> -->
-      </ul>
-        <td><span>学生姓名</span>
-        <tr v-for="(item, index) in studentlist" :key="index">
-          {{item.stu_name}}
-        </tr>
-      </td>
-      <td><span>期末成绩</span>
-        <tr v-for="(item, index) in studentlist" :key="index">
-          {{item.stu_score}}
-        </tr>
-      </td>
-      <td><span>操作</span>
-        <tr  v-for="(item, index) in studentlist" :key="index" :index="index">
-          <button type="button" @click="delstudet(index)">删除</button>
-        </tr>
-      </td>
-      <td>
-        <tr>
+      </div>
+      <div class="divtd">
+        <td>
+          <span>学生姓名</span>
+            <tr v-for="(item, index) in studentlist" :key="index">
+              {{item.stu_name}}
+            </tr>
+        </td>
+        <td>
+          <span>期末成绩</span>
+            <tr v-for="(item, index) in studentlist" :key="index">
+              {{item.stu_score}}
+            </tr>
+        </td>
+        <td>
+          <span>操作</span>
+            <tr  v-for="(item, index) in studentlist" :key="index" :index="index">
+              <button type="button" @click="delstudet(index)">删除</button>
+            </tr>
+        </td>
+      </div>
+      <tr>
+        <td>
           <span id="student_name">学生姓名</span>
           <input type="text" v-model="student_name">
-        </tr>
-      </td>
-      <td>
-        <tr>
+        </td>
+      </tr>
+      <tr>
+        <td>
           <span id="student_score">期末成绩</span>
           <input type="text" v-model="student_score">
-        </tr>
-      </td>
+        </td>
+      </tr>
       <button type="button" @click="addstudent()">添加</button>
   </div>
 </template>
@@ -80,6 +85,26 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+*{
+  margin-left: 30px;
+  padding: 0;
+  list-style: none;
+}
+.index {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+#user {
+  margin-left: 150px;
+}
+#logout {
+  color: #03a9f4;
+  margin: 0;
+}
+.divtd {
+  width: 310px;
+  border: 1px solid #9e9e9e;
+  color: #009688;
+}
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,42 +1,60 @@
 <template>
   <div class="login">
-    <form v-if='log'>
+    <form v-if='log' class="text">
       <tr>
         <td>
-          <span id="user">用户名</span>
-          <input type="text" v-model="username">
+          <div class="divtd">
+            <span id="user">用户名</span>
+            <input type="text" v-model="username">
+          </div>
         </td>
       </tr>
       <tr>
         <td>
-          <span id="pwd">密码</span>
-          <input type="text" v-model="password">
         </td>
       </tr>
-      <button @click="login()">登录</button>
-      <button @click="reg()">注册</button>
+      <tr>
+        <td>
+          <div class="divtd">
+            <span id="pwd">密码</span>
+            <input type="text" v-model="password">
+          </div>
+        </td>
+      </tr>
+      <div class="btn">
+        <button type="button" @click="login()">登录</button>
+        <button type="button" @click="reg()">注册</button>
+      </div>
     </form>
-    <form v-else>
+    <form v-else class="text">
       <tr>
         <td>
-          <span id="user">用户名</span>
-          <input type="text" id="username" name="username" v-model="username">
+          <div class="divtd">
+            <span id="user">用户名</span>
+            <input type="text" id="username" name="username" v-model="username">
+          </div>
         </td>
       </tr>
       <tr>
         <td>
-          <span id="pwd">密码</span>
-          <input type="text" id="password" name="password" v-model="password">
+          <div class="divtd">
+            <span id="pwd">密码</span>
+            <input type="text" id="password" name="password" v-model="password">
+          </div>
         </td>
       </tr>
       <tr>
         <td>
-          <span id="repwd">确认密码</span>
-          <input type="text" id="repassword" name="repassword" v-model="repassword">
+          <div class="divtd">
+            <span id="repwd">确认密码</span>
+            <input type="text" id="repassword" name="repassword" v-model="repassword">
+          </div>
         </td>
       </tr>
-      <button type="button" @click="addUer()">确定</button>
-      <button type="button" @click="cancel()">取消</button>
+      <div class="btn">
+        <button type="button" @click="addUer()">确定</button>
+        <button type="button" @click="cancel()">取消</button>
+      </div>
     </form>
   </div>
 </template>
@@ -90,7 +108,54 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+*{
+  margin: 0;
+  padding: 0;
+}
 .login {
-  margin:auto
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  height: 220px;
+  width: 360px;
+  margin-top: -100px;
+  margin-left: -100px;
+  background-color: #0084cd;
+}
+.text {
+  margin: 30px 50px;
+  font-family: Arial;
+  font-size: 18px;
+  font-weight: bold;
+}
+.divtd {
+  border: 1px solid #dadadaed;
+  padding: 5px;
+  color: white;
+}
+td {
+  padding: 4px;
+}
+input {
+  width: 155px;
+  height: 20px;
+  float: right;
+  font-family: "Microsoft soft";
+  // outline: 0;
+  // border: 1px solid yellow;
+  // border-radius: 5px;
+}
+.btn {
+  margin-left: 35px;
+  margin-top: 25px;
+}
+button {
+  cursor: pointer;
+  margin-left: 20px;
+  width: 55px;
+  height: 25px;
+  font-size: 15px;
+  border-radius: 5px;
+  background-color: #e7e4e4;
 }
 </style>
